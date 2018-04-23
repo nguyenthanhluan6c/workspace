c4e1e9ec86f9abb8276bae12d5da38b16541a0a6
Nguyen Thanh Luan <nguyenthanhluan6c@gmail.com>
2018-04-23 15:47:10 +0700

    use_rbenv

--- profile.zsh
+++ profile.zsh
@@ -16,3 +16,19 @@ export PATH=~/.npm-global/bin:$PATH
 
 eval $(thefuck --alias)
 eval "$(direnv hook zsh)"
+
+export PATH="$HOME/.rbenv/bin:$PATH"
+eval "$(rbenv init -)"
+
+# gem env home
+# should be /home/nguyenthanhluan/.rbenv/versions/2.4.2/lib/ruby/gems/2.4.0
+
+# Uninstall rbenv
+# sudo apt-get remove --auto-remove rbenv
+
+# export PATH="$PATH:$HOME/.rvm/bin"
+# [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
+
+# Uninstall RVM
+# rvm implode
+# gem uninstall rvm
